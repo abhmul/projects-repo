4b14c37989cc3bb62cd104c17dab7771d41628a6
abhmul <abhmul@gmail.com>
2019-12-26 22:28:46 -0500

    added nearest point labelign

--- models/perceptronnet.py
+++ models/perceptronnet.py
@@ -22,6 +22,103 @@ class SignLayer(tf.keras.layers.Dense):
         return activation, strength
 
 
+class PerceptronNet2(tf.keras.Model):
+    def __init__(self, n_hidden):
+        super(PerceptronNet2, self).__init__()
+        print("This PerceptronNet has only 1 output!")
+        print("This PerceptronNet has only 1 layer!")
+        self.hidden_layer = SignLayer(n_hidden)
+        self.final_layer = SignLayer(1)
+        print("Not building layers!")
+
+    def call(self, input_tensor, training=False):
+        activations = []
+        strengths = []
+        x = input_tensor
+        for layer in [self.hidden_layer] + [self.final_layer]:
+            x, s = layer(x)
+            activations.append(x)
+            strengths.append(x)
+
+        return {"activations": activations, "strengths": strengths}
+
+    def pred(self, input_tensor):
+        return self(input_tensor)["activations"][-1]
+
+    @staticmethod
+    def compute_hidden_label(activation_tensor, label_tensor, kernel, bias):
+        # activation tensor is B x I
+        # label tensor is B x 1
+        # kernel is I x 1
+        # bias is 1
+
+        # Sort the kernel by absolute value and use that to sort activations
+        top_kernel_inds = tf.argsort(
+            tf.abs(kernel)[:, 0], direction="DESCENDING", axis=0
+        )
+        kernel_sorted = tf.gather(tf.transpose(kernel), top_kernel_inds, axis=1)
+        activation_sorted = tf.gather(activation_tensor, top_kernel_inds, axis=1)
+        # Get cumulative sum of switching signs to match kernel, and keeping them the same
+        kernel_cumsum = tf.matmul(
+            label_tensor, tf.cumsum(tf.abs(kernel_sorted), exclusive=True, axis=1)
+        )
+        strength_cumsum = tf.cumsum(
+            activation_sorted * kernel_sorted, reverse=True, axis=1
+        )
+        # Pad and add to get weight dot activation with switching first k bits
+        resultant = kernel_cumsum + strength_cumsum + bias
+        # Flip the bits until label * result > 0
+        bit_keep_mask = tf.cast((label_tensor * resultant) > 0, dtype=tf.float32)
+        activation_label_sorted = bit_keep_mask * activation_sorted + (
+            1 - bit_keep_mask
+        ) * tf.matmul(label_tensor, tf.sign(kernel_sorted))
+        # Undo the sort to recover the activation labels
+        unsort_inds = tf.argsort(top_kernel_inds)
+        activation_label_tensor = tf.gather(
+            activation_label_sorted, unsort_inds, axis=1
+        )
+        assertions.assert_is_signs(activation_label_tensor)
+
+        return activation_label_tensor
+
+    def update(self, input_tensor, label_tensor, learning_rate=1.0):
+        batch_size = input_tensor.shape[0]
+        output = self(input_tensor)
+        prediction = output["activations"][-1]  # B x 1
+
+        # Get the internal labels  B x H
+        activations = output["activations"][-2]  # B x H
+        internal_labels = self.compute_hidden_label(
+            activations, label_tensor, self.final_layer.kernel, self.final_layer.bias
+        )
+
+        # Hidden update
+        hidden_error = 1 / 2 * (internal_labels - activations)  # B x H
+        avg_input_size = (tf.reduce_sum(tf.abs(input_tensor)) / batch_size) + 1
+        hidden_weight_update = (
+            tf.matmul(tf.transpose(input_tensor), hidden_error)
+            / batch_size
+            / avg_input_size
+        )
+        hidden_bias_update = tf.reduce_mean(hidden_error, axis=0) / avg_input_size
+
+        # Final update
+        output_error = 1 / 2 * (label_tensor - prediction)  # B x 1
+        avg_activations_size = (tf.reduce_sum(tf.abs(activations)) / batch_size) + 1
+        final_weight_update = (
+            tf.matmul(tf.transpose(activations), output_error)
+            / batch_size
+            / avg_activations_size
+        )
+        final_bias_update = tf.reduce_mean(output_error, axis=0) / avg_activations_size
+
+        # Perform the updates
+        tf.assign_add(self.hidden_layer.kernel, learning_rate * hidden_weight_update)
+        tf.assign_add(self.hidden_layer.bias, learning_rate * hidden_bias_update)
+        tf.assign_add(self.final_layer.kernel, learning_rate * final_weight_update)
+        tf.assign_add(self.final_layer.bias, learning_rate * final_bias_update)
+
+
 class PerceptronNet(tf.keras.Model):
     def __init__(self, n_hiddens, regularization=0.0):
         super(PerceptronNet, self).__init__()
