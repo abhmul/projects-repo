527f80a4e61d1cb42374600c1d0357dbc16508b2
abhmul <abhmul@gmail.com>
2020-07-13 12:59:16 -0700

    complete find & replace

--- projectsrepo/scripts/find_replace.py
+++ projectsrepo/scripts/find_replace.py
@@ -1,8 +1,12 @@
+import re
+
 if __name__ == "__main__":
-    original_string = input()
+    original_string = input("Original String: ")
     print(f"Original:\n{original_string}")
-    find_regex = input().ra
+
+    find_regex = re.compile(input("Find Regex: "))
     print(f"Find:\n{find_regex}")
-    replace_regex = input()
+    replace_string = input("Replace String (use \\n for backrefs): ")
     print(f"Replace:\n{replace_regex}")
-    re.sub(find_regex)
+    result = re.sub(find_regex, replace_string, original_string)
+    print(f"Result:\n{result}")
